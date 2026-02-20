7fd137a9d6bc0208fb1a72ffbd469e085f19af87
Dr. Thomas <75437150+tthoma81Debug@users.noreply.github.com>
2026-02-20 12:23:47 -0600

    preclass

---
+++ examples/liveDemo2.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<!-- WARNING: This example intentionally contains errors. This is not the final version. -->
+<head>
+<title>Demo 1</title>
+    <!-- Required meta tags -->
+<meta charset="utf-8">
+<link rel ="stylesheet" href="mainCss.css">
+<style>
+</style>
+
+<script src="mainJs.js"></script>
+
+</head>
+
+<body>
+    <div>
+      <h1 class = "exampleClass"  onclick = "exampleFunction()">
+        Demo Page
+      </h1>
+        <div class ="exampleClass">
+            <p>
+                This is a demo page.
+            </p>
+            <form method="post" action="endPoint1.php">
+                <input type="text" name="data" placeholder="Placeholder">
+                <button type="button" onclick="exampleFunction()">Actual Button</button>
+            </form>
+
+        </div>
+    </div>
+
+<?php
+    echo "Hello, World!" . "php is working! version is " . phpversion();
+?>
+
+
+
+</body>
+</html>
