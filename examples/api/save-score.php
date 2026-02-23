9241670e82ef7e459209cc3de66331c5dffc36d2
Dr. Thomas <75437150+tthoma81Debug@users.noreply.github.com>
2026-02-23 13:51:32 -0600

    code at end of class on 02/23

--- examples/api/save-score.php
+++ examples/api/save-score.php
@@ -3,17 +3,25 @@
     //header('Content-Type: application/json');
 
     //we have a bug here. Can you spot it?
-    $name = $_POST ['name'];
-    $score = $_POST ['score'];
+    //$name = $_POST ['name'];
+    //$score = $_POST ['score'];
 
+    //The whole request body
+    $requestBody = file_get_contents('php://input');
+
+    /*
     $response = [
         'success' => true,
         'message' => "Got the score...but buggy :)",
         'echoName' => $name, //will be null
         'echoScore' => $score //will be null
     ];
+    */
 
-    echo $response //this will not work as intended but it is a start
+    echo "server just received a request. The data it heard was " . $requestBody;
+
+    
+    //echo $response //this will not work as intended but it is a start
     
     //echo "Hello this is endpoint 1 speaking" . "php is working! version is " . phpversion();
 ?>
