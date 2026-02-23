b10b03d53fb24fb343f56fb7f971067270ced29c
Dr. Thomas <75437150+tthoma81Debug@users.noreply.github.com>
2026-02-23 12:02:55 -0600

    added an example of incorrect timing commented out
    don't want to waste time typing it in class, but want to show it so here it is

--- examples/fetchExampleJS.js
+++ examples/fetchExampleJS.js
@@ -15,11 +15,30 @@ function sendScore(){
     }).catch(error=> {
         console.error("error in fetch");
     })
+        
+
+    /*
+  //Note: This is intentionally broken. Can you tell why? Hint: 'timing' is important here.
+  var Response = fetch('./api/save-score.php',{
+        method: 'POST',
+        body: payload
+    })
+    
+    handleResponse(Response); 
+    */
 
 }
     
-    function setupStuff(){
+    function setupStuff()
+    {
         console.log("setup function called");
         sendScore();
         console.log("reached end of setup function");
     }
+
+    /*
+    function handleResponse(response)
+    {
+        console.log("handleResponse called. response is: ", response);
+    }
+    */
