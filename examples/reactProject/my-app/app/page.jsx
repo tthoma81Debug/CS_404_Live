23741b4a1fc767a9368ebc9b3f10c087fb40945d
Dr. Thomas <75437150+tthoma81Debug@users.noreply.github.com>
2026-04-17 13:15:39 -0500

    first react project

---
+++ examples/reactProject/my-app/app/page.jsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+
+
+
+
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://react.dev/images/docs/scientists/yXOvdOSs.jpg',
+  imageSize: 90,
+};
+
+export default function Home() {
+  return (
+    <>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
+    </>
+  );
+}
